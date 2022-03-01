6726f3898619cda39f1b4d8794a106b628a4b0ab
Riley Croker <riley.croker@vikings.berry.edu>
2022-02-28 20:41:48 -0500

    Added the frames state

--- PopArtPCBCode/PopArtPCBCode.ino
+++ PopArtPCBCode/PopArtPCBCode.ino
@@ -1,27 +1,75 @@
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel strip(30, 0, NEO_GRB + NEO_KHZ800);
 
+//button stuff
 const int buttonPin = 1;
-
 boolean prevButState = false;
 boolean currentButState = false;
 
+// finite state machine
 int state = 0;
+int numGrid = 26;
+int numFrames = 3;
+
+char index[26]{
+  // Insert Index Values Here
+       5, 10, 15, 20, 
+    1, 6, 11, 16, 21, 26,
+    2, 7, 12, 17, 22, 27,
+    3, 8, 13, 18, 23, 28,
+       9, 14, 19, 24      
+};
+
+char colors[6][3]{
+  // Create 3 Colors Here
+  { 255, 0, 0 },
+  { 0, 0, 0},
+  { 255, 255, 255 },
+  { 0, 0, 255 },
+  { 255, 255, 0 },
+  { 20, 20, 20 }
+};
+
+char frames[3][26]{
+  // Create 2 Animation Frames Here
+  {
+       0, 0, 0, 0, 
+    0, 0, 1, 1, 0, 0,
+    1, 1, 0, 0, 1, 1,
+    2, 2, 1, 1, 2, 2,
+       2, 2, 2, 2  
+  },
+  {
+       0, 3, 3, 0, 
+    3, 3, 1, 1, 3, 3,
+    1, 1, 3, 3, 1, 1,
+    2, 2, 1, 1, 2, 2,
+       2, 2, 2, 2  
+  },
+  {
+       4, 4, 4, 4, 
+    5, 4, 1, 1, 4, 5,
+    1, 1, 4, 4, 1, 1,
+    2, 2, 1, 1, 2, 2,
+       2, 2, 2, 2  
+  }
+};
 
 void setup() {
   pinMode(buttonPin, INPUT);
   strip.begin();
   strip.show();
-  strip.setBrightness(30);
+//  strip.setBrightness(30);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   currentButState = debounce(buttonPin,prevButState);
 
-  if(currentButState == HIGH && prevButState == LOW)
+  if(currentButState == HIGH && prevButState == LOW) {
+    strip.clear();
     state = (state+1) % 3;
-
+  }
   switch (state) {
     case 0:
       for(int i = 0; i < strip.numPixels(); i++) {
@@ -34,8 +82,20 @@ void loop() {
       }
     break;
     case 2:
-      for(int i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i,0,255,0);
+      for ( int j = 0; j < numFrames; j++ ) {
+        int whichFrame = j;
+        for( int i = 0; i < numGrid; i++ ) {
+          int whichLED = i;
+          int whichIndex = index[whichLED];
+          int whichColor = frames[whichFrame][whichLED];
+    
+          int r = colors[whichColor][0];
+          int g = colors[whichColor][1];
+          int b = colors[whichColor][2];
+          strip.setPixelColor(whichIndex,r,g,b);
+        }
+        strip.show();
+        delay(1000);
       }
     break;
   }
