416a6e207c6cd394399600096b87567efd6901c1
Riley Croker <riley.croker@vikings.berry.edu>
2022-02-28 19:48:55 -0500

    Added stubs for the finite state machine

--- PopArtPCBCode/PopArtPCBCode.ino
+++ PopArtPCBCode/PopArtPCBCode.ino
@@ -2,7 +2,11 @@
 Adafruit_NeoPixel strip(30, 0, NEO_GRB + NEO_KHZ800);
 
 const int buttonPin = 1;
-int buttonState = 0;
+
+boolean prevButState = false;
+boolean currentButState = false;
+
+int state = 0;
 
 void setup() {
   pinMode(buttonPin, INPUT);
@@ -13,11 +17,39 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonState = digitalRead(buttonPin);
+  currentButState = debounce(buttonPin,prevButState);
 
-  if(buttonState == 1) 
-    strip.setPixelColor(0,0,255,0);
-  else
-    strip.setPixelColor(0,255,0,0);
+  if(currentButState == HIGH && prevButState == LOW)
+    state = (state+1) % 3;
+
+  switch (state) {
+    case 0:
+      for(int i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i,0,0,0);
+      }
+    break;
+    case 1:
+      for(int i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i,255,0,0);
+      }
+    break;
+    case 2:
+      for(int i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i,0,255,0);
+      }
+    break;
+  }
+
   strip.show();
+
+  prevButState = currentButState; 
+}
+
+
+boolean debounce(int buttonPin, boolean preState)
+{
+  boolean buttonState = digitalRead(buttonPin);
+  if (buttonState != preState)
+    delay(50);
+  return buttonState;
 }
